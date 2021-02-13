0450adf5e4ec21e2ce5b8fe7cfa92ff3e0c80735
Bartosz_Szmyt <bartoszszmyt0@gmail.com>
2021-02-13 16:25:43 +0100

    book_details skeleton complete, tested

--- test_db_functions.py
+++ test_db_functions.py
@@ -2,6 +2,7 @@
 
 import unittest
 from buttons import db_functions as db_func
+from ddt import ddt, data
 import psycopg2 as db
 
 class TestContextManager(unittest.TestCase):
@@ -17,7 +18,7 @@ class TestContextManager(unittest.TestCase):
         self.assertTrue(postgres.conn.closed)
         self.assertTrue(postgres.cursor.closed)
 
-    
+@ddt   
 class TestDbFunctions(unittest.TestCase):
     """Test all functions that make contact whith db"""
  
@@ -28,31 +29,47 @@ class TestDbFunctions(unittest.TestCase):
     'host' : '192.168.1.24',
     'port' : '5432'
     }
-     
+    
+    TEST_DATA_LISTS = (('Title','Author','Kind','Publisher',1993,'Language',100,'ISBN'),
+                       ('Title','Author','Kind','Publisher','','Language',100,'ISBN'),
+                       ('Title','','Kind','Publisher',1993,'Language',100,'ISBN'),
+                       ('','Author','Kind','Publisher',1993,'Language',100,'ISBN'),
+                       ('Title','Author','Kind','',1993,'Language',100,'ISBN'))
+    
     def setUp(self):
         self.conn = db.connect(**self.TEST_PARAMS) 
         self.cursor = self.conn.cursor()    
-       
-    def test_save_book(self):
+    
+    @data(*TEST_DATA_LISTS)
+    def test_save_book(self, data_list):
         """Test save_book() function on test database"""
         
-        # Create some test data to save
-        self.test_data = ('Title','Author','Kind','Publisher',1993,
-                          'Language',100,'ISBN')
+        # Get test data
+        self.test_data = data_list
         
         # Save it to database using function from db_functions
-        db_func.save_book(self.test_data, conn_params=self.TEST_PARAMS)
+        try:
+            db_func.save_book(self.test_data, conn_params=self.TEST_PARAMS)
         
-        # Check saved record
-        self.cursor.execute('SELECT * FROM publications WHERE lib_id > 100;')
-        results = self.cursor.fetchone()
-        self.assertEqual(self.test_data, results[1:9])
+            # Check saved record
+            self.cursor.execute('SELECT * FROM publications WHERE lib_id > 100;')
+            results = self.cursor.fetchone()
+            self.assertEqual(self.test_data, results[1:9])
+            
+            # Delete created test_record
+            self.cursor.execute('DELETE FROM publications WHERE lib_id > 100;')
+            self.conn.commit()
         
-        # Delete created test_record
-        self.cursor.execute('DELETE FROM publications WHERE lib_id > 100;')
-        self.conn.commit()
-        
-        # Test if function handles expectations properly
+        # Check expections handling
+        except db.IntegrityError:
+            for item in self.test_data:
+                if item == '':
+                    self.index = self.test_data.index(item)
+            
+            self.assertIn(self.index, (0,1,3,4))
+            
+        except db.DataError:
+            self.assertEqual(self.test_data[4],'')
     
     def tearDown(self):
         self.cursor.close()
